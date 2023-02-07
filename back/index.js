fabf72db1e3ef209363096439b0a50499441b05c
Florian Latapie <florian.latapie@etu.unice.fr>
2023-02-07 23:32:21 +0100

    refactor back/index le code est propre maintenant

--- back/index.js
+++ back/index.js
@@ -1,17 +1,15 @@
 // The http module contains methods to handle http queries.
 import * as http from 'http';
 // Let's import our logic.
-//const fileQuery = require('./queryManagers/front.js')
 import * as fileQuery from './queryManagers/front.js'
 import * as apiQuery from './queryManagers/api.js'
-//const apiQuery = import('./queryManagers/api.js')
 import userdb from "./database/userdb.js";
-//const { Server } = require("socket.io");
-// convert this require to an import
 import {Server} from "socket.io";
 import Player from "../front/GameLogic/Player.js";
 import GameEngine from "../front/GameLogic/GameEngine.js";
 import computeMove from "./logic/ai.js";
+
+// Servers setup -------------------------------------------------------------------------------------------------------
 
 /* The http module contains a createServer function, which takes one argument, which is the function that
 ** will be called whenever a new request arrives to the server.
@@ -59,53 +57,67 @@ const io = new Server(httpServer, {
     }
 });
 
-let AIPlay = function (AIPlayer, gameEngine) {
-    let globalCoordinatesAI = computeMove(gameEngine);
-    let column = globalCoordinatesAI[0];
-    let row = globalCoordinatesAI[1];
-    let gameState = gameEngine.playTurn(AIPlayer, column, row);
+// Methods -------------------------------------------------------------------------------------------------------------
+let playerPlay = function (player, gameEngine, column, row) {
+    let gameState = gameEngine.playTurn(player, column, row);
     gameSocket.emit("updatedBoard", gameEngine.grid)
 
     if (gameState.isFinished === true) {
         gameSocket.emit("gameIsOver", gameState.winner)
     }
 }
+let AIPlay = function (AIPlayer, gameEngine) {
+    let globalCoordinatesAI = computeMove(gameEngine); // computeMove from ai.js : [column, row]
+    let column = globalCoordinatesAI[0];
+    let row = globalCoordinatesAI[1];
+
+    playerPlay(AIPlayer, gameEngine, column, row)
+}
+
+let humanPlay = function (HumanPlayer, gameEngine, globalCoordinates){
+    let column = globalCoordinates[0];
+    let row = globalCoordinates[1];
+
+    playerPlay(HumanPlayer, gameEngine, column, row)
+}
+
+// DB methods ----------------------------------------------------------------------------------------------------------
+let addUser = async function (user) {
+    await userdb.addUser(user);
+}
+
+
+// main ----------------------------------------------------------------------------------------------------------------
 
 const gameSocket = io.of("/api/game")
 let gameEngine;
 
+// Connection ----------------------------------------------------------------------------------------------------------
 gameSocket.on('connection', (socket) => {
     console.log('user ' + socket.id + ' connected');
     let AIPlayer = new Player("AI", 0)
     let HumanPlayer = new Player("HumanPlayer", socket.id)
 
-    socket.on("setup", obj => {
-        console.log("setup", obj);
-        if (obj.AIplays !== 1 && obj.AIplays !== 2) {
+    // Setup ----------------------------------------------------------------------------------------------------------
+    socket.on("setup", setupObject => {
+        console.log("setup", setupObject);
+        if (setupObject.AIplays !== 1 && setupObject.AIplays !== 2) {
             gameSocket.emit("errorSetUp", new Error("Invalid setup"))
         }
 
-        if (obj.AIplays === 1) {
+        if (setupObject.AIplays === 1) {
             gameEngine = new GameEngine(AIPlayer, HumanPlayer);
             AIPlay(AIPlayer, gameEngine);
-
         } else {
             gameEngine = new GameEngine(HumanPlayer, AIPlayer);
         }
     });
 
+    // newMove ---------------------------------------------------------------------------------------------------------
     socket.on("newMove", (globalCoordinates) => {
         console.log("newMove", globalCoordinates);
         try {
-            let column = globalCoordinates[0];
-            let row = globalCoordinates[1];
-
-            let gameState = gameEngine.playTurn(HumanPlayer, column, row)
-
-            gameSocket.emit("updatedBoard", gameEngine.grid)
-            if (gameState.isFinished === true) {
-                gameSocket.emit("gameIsOver", gameState.winner)
-            }
+            humanPlay(HumanPlayer, gameEngine, globalCoordinates);
             AIPlay(AIPlayer, gameEngine);
         } catch (e) {
             console.log(e);
@@ -114,11 +126,8 @@ gameSocket.on('connection', (socket) => {
         }
     })
 
+    // disconnect ------------------------------------------------------------------------------------------------------
     socket.on('disconnect', () => {
-        console.log('user disconnected');
+        console.log('user ' + socket.id + ' disconnected');
     });
 });
-
-let addUser = async function (user) {
-    await userdb.addUser(user);
-}
