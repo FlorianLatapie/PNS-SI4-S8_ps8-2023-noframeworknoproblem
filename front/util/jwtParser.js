fce1c9dcb0862ea15786d8379c65c7d447779e42
Florian Latapie <florian.latapie@etu.unice.fr>
2023-03-07 11:12:27 +0100

    correction erreur "silencieuse" parsing mauvais ou token vide

--- front/util/jwtParser.js
+++ front/util/jwtParser.js
@@ -1,4 +1,6 @@
 function parseJwt(token) {
+    if (token === null || token.indexOf(".") === -1)
+        return null;
     let base64Url = token.split('.')[1];
     let base64 = base64Url.replace('-', '+').replace('_', '/');
     return JSON.parse(window.atob(base64));
@@ -6,6 +8,9 @@ function parseJwt(token) {
 
 function isTokenValid(token) {
     let parsedJwt = parseJwt(token);
+    if (parsedJwt === null)
+        return false;
+    
     let expirationTime = parsedJwt.exp;
     let currentTime = Date.now() / 1000;
     return currentTime < expirationTime;
