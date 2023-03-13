af4b2a171611f48045e0ec50ef994cd6f850a4cf
Florian Latapie <florian.latapie@etu.unice.fr>
2023-03-14 00:06:53 +0100

    la ca marche

--- back/database/userstatsdb.js
+++ back/database/userstatsdb.js
@@ -54,7 +54,7 @@ class UserStatsDb {
             if (await this.existsStatsForThisUser(userId)){
                 return await this.usersStats.findOne({userId});
             } else {
-                return null ;
+                return {};
             }
         } catch (error) {
             console.error(error);
