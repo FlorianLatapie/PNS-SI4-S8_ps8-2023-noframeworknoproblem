f21656c23aa9a2c2ddf4b87afb87ede98b0f7520
Florian Latapie <florian.latapie@etu.unice.fr>
2023-03-13 16:16:11 +0100

    affichage des infos minimales sur le profil utilisateur

---
+++ front/profile/profile.js
@@ -0,0 +1,4 @@
+"use strict";
+
+document.getElementById("displayed-username").innerText = localStorage.getItem("username");
+document.getElementById("unique-id").innerText = localStorage.getItem("userId");
