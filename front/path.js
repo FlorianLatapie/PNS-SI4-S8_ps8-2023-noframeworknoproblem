7d316e511e897f812b410c9e4252ddba50b78471
Florian Latapie <florian.latapie@etu.unice.fr>
2023-03-26 22:33:49 +0200

    réorganisation du path.js

--- front/path.js
+++ front/path.js
@@ -1,30 +1,51 @@
 "use strict";
 const BASE_URL = window.location.protocol+ "//" + window.location.host + "/";
+
+// Pages ---------------------------------------------------------------------------------------------------------------
 const SIGNUP_URL = 'signup/';
 const LOGIN_URL = 'login/';
 
-const PROFILE_URL = 'profile/';
-
+/// Pages when logged in -----------------------------
 const HOME_URL = 'home/';
+const PROFILE_URL = 'profile/';
+const USER_PROFILE_URL = 'userProfile/';
+const NOTIFICATIONS_URL = 'notificationCenter/';
+const FRIENDS_URL = "friends/";
+const SEARCH_USERS_URL = 'searchUsers/';
 
+
+//// Game pages --------------------------------------
+///// local
 const PLAY_LOCAL_URL = 'play/local/';
 
+///// AI
 const PLAY_AI_URL = 'play/ai/';
 const PLAY_AI_ORDER_URL = 'play/ai/order/';
 
+///// Matchmaking
 const PLAY_MATCHMAKING_URL = 'play/matchmaking/';
 
-const SEARCH_USERS_URL = 'searchUsers/';
-
-const NOTIFICATIONS_URL = 'notificationCenter/';
-
+// API -----------------------------------------------------------------------------------------------------------------
 const API_URL = 'api/';
-
 const ACHIEVEMENTS_URL = "achievements/";
-
-const FRIENDS_URL = "friends/";
-
 const USERS_URL = "users/";
 
-export {BASE_URL, SIGNUP_URL, LOGIN_URL, PROFILE_URL, HOME_URL, PLAY_LOCAL_URL, PLAY_AI_URL, PLAY_AI_ORDER_URL,
-    PLAY_MATCHMAKING_URL, API_URL, ACHIEVEMENTS_URL, FRIENDS_URL, USERS_URL, SEARCH_USERS_URL, NOTIFICATIONS_URL};
+// export all the constants --------------------------------------------------------------------------------------------
+export {
+    BASE_URL,
+    SIGNUP_URL,
+    LOGIN_URL,
+    HOME_URL,
+    PROFILE_URL,
+    USER_PROFILE_URL,
+    NOTIFICATIONS_URL,
+    FRIENDS_URL,
+    SEARCH_USERS_URL,
+    PLAY_LOCAL_URL,
+    PLAY_AI_URL,
+    PLAY_AI_ORDER_URL,
+    PLAY_MATCHMAKING_URL,
+    API_URL,
+    ACHIEVEMENTS_URL,
+    USERS_URL
+};
