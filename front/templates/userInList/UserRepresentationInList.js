0b7dd79635eb2eb6e1102ce40589558a0769a964
Florian Latapie <florian.latapie@etu.unice.fr>
2023-03-26 22:48:45 +0200

    debut de la page ami

--- front/templates/userInList/UserRepresentationInList.js
+++ front/templates/userInList/UserRepresentationInList.js
@@ -1,6 +1,6 @@
 "use strict";
 
-import {BASE_URL, HOME_URL} from "../../path.js";
+import {BASE_URL, PROFILE_URL, USER_PROFILE_URL} from "../../path.js";
 
 function createUserPreviewDiv(userObj) {
     const container = document.createElement('div');
@@ -9,7 +9,12 @@ function createUserPreviewDiv(userObj) {
 
     // TODO change the link to redirect to the user page
     container.addEventListener("click", () => {
-        window.location.replace(BASE_URL + HOME_URL)
+        let destination = BASE_URL + USER_PROFILE_URL + "?userId=" + userObj.userId;
+
+        if (localStorage.getItem("userId") === userObj.userId){
+            destination = BASE_URL + PROFILE_URL;
+        }
+        window.location.replace(destination);
     })
 
     container.classList.add("user-profile");
