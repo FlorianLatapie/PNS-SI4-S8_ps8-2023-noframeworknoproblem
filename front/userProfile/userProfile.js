4a2bc7feb0c99355a8a7c396937bf87dda6e2eaa
Florian Latapie <florian.latapie@etu.unice.fr>
2023-03-27 23:40:14 +0200

    il faut que je change le bouton automatiquement

--- front/userProfile/userProfile.js
+++ front/userProfile/userProfile.js
@@ -1,20 +1,36 @@
 "use strict";
 
 import {BASE_URL} from "../util/frontPath.js";
+import {ACHIEVEMENTS_URL, API_URL, FRIENDS_URL, USERS_URL} from "../util/path.js";
 
 // Script --------------------------------------------------------------------------------------------------------------
 
-// user id is in the url : http://localhost:8000/userprofile/?userId=XXX
-// parse the url to get the id
-
 let url = new URL(window.location.href);
-let userId = url.searchParams.get("userId");
-let username = userId;
+let userIdOfThisPage = url.searchParams.get("userId");
 
-if (!isUserValid(userId)) {
-    alert("User '" + userId + "' not found, redirecting to home page...");
-    window.location.replace(BASE_URL);
+if (!isUserValid(userIdOfThisPage)) {
+    whenError();
 }
+
+let userPromise = fetch(BASE_URL + API_URL + USERS_URL + "get/" + userIdOfThisPage, {
+    method: 'get', headers: {
+        'Authorization': 'Bearer ' + localStorage.getItem('token'),
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+    }
+}).then((response) => {
+    if (!response.ok) {
+        throw new Error("Error while calling API" + response.status)
+    }
+    return response.json();
+}).then((data) => {
+    return data;
+}).catch(error => {
+    whenError();
+});
+
+let user = await userPromise;
+let username = user.username;
 
 let title = document.getElementById("page-title");
 title.innerHTML = "Profil de " + username;
@@ -22,8 +38,183 @@ title.innerHTML = "Profil de " + username;
 let greeting = document.getElementById("greeting");
 greeting.innerHTML = "Bienvenue sur la page de " + username + " !";
 
+let achievementsPromise = fetch(BASE_URL + API_URL + ACHIEVEMENTS_URL + "getAll/", {
+    method: "post", headers: {
+        'Accept': 'application/json', 'Content-Type': 'application/json'
+    }, body: JSON.stringify({token: localStorage.getItem("token"), userId: userIdOfThisPage})
+}).then(async (response) => {
+    if (!response.ok) {
+        throw new Error("Error while calling API" + response.status)
+    }
+    return response.json();
+}).then((data) => {
+    return data;
+}).catch(error => {
+    console.log(error);
+});
+
+// friends -------------------------------------
+
+async function updateButton() {
+    let friendshipButton = document.getElementById("friendship-button");
+
+    let status = await getStatus(localStorage.getItem("userId"), userIdOfThisPage)
+
+    console.log(status);
+
+    switch (status) {
+        case "request":
+            friendshipButton.innerHTML = "Annuler la demande d'ami";
+            friendshipButton.addEventListener("click", async () => {
+                cancelRequest(userIdOfThisPage);
+            });
+            break;
+        case "friend":
+            friendshipButton.innerHTML = "Retirer de mes amis";
+
+            friendshipButton.addEventListener("click", async () => {
+                removeFriend(userIdOfThisPage);
+            });
+            break;
+        case "pending":
+            friendshipButton.innerHTML = "Accepter la demande";
+
+            friendshipButton.addEventListener("click", async () => {
+                cancelRequest(userIdOfThisPage);
+            });
+            break;
+
+        case "none":
+            friendshipButton.innerHTML = "Ajouter aux amis";
+            friendshipButton.addEventListener("click", async () => {
+                addFriend(userIdOfThisPage);
+            });
+            break;
+    }
+}
+
+await updateButton();
+
+// achievements -------------------------------------
+
+let achievements = await achievementsPromise;
+
+if (achievements.length === 0) {
+    let success = document.getElementById("successes");
+    success.innerHTML = "Aucun succès débloqué pour le moment.";
+}
+
+let achievementsDiv = document.getElementById("achievements");
+achievements.forEach(achievement => {
+    let achievementDiv = document.createElement("li");
+    achievementDiv.classList.add("achievement");
+    achievementDiv.innerText = achievement.achievementId + " - " + "débloqué (100%)";
+    achievementsDiv.appendChild(achievementDiv);
+});
 
 // Functions -----------------------------------------------------------------------------------------------------------
 function isUserValid(userId) {
     return Boolean(userId);
+}
+
+function whenError() {
+    alert("User '" + userIdOfThisPage + "' not found, redirecting to home page...");
+    window.location.replace(BASE_URL);
+}
+
+function getStatus(myId, userId) {
+    return fetch(BASE_URL + API_URL + FRIENDS_URL + 'friendshipStatus' + "/" + userId, {
+        method: 'get', headers: {
+            'Authorization': 'Bearer ' + localStorage.getItem('token'),
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        }
+    }).then((response) => {
+        if (!response.ok) {
+            throw new Error("Error while calling API (button) " + response.status)
+        }
+        return response.text();
+    }).then((data) => {
+        return data;
+    }).catch(error => {
+        console.log(error);
+    });
+}
+
+function acceptFriendRequest(userIdOfThisPage) {
+    return fetch(BASE_URL + API_URL + FRIENDS_URL + 'accept' + "/" + userIdOfThisPage, {
+        method: 'post', headers: {
+            'Authorization': 'Bearer ' + localStorage.getItem('token'),
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        }
+    }).then((response) => {
+        if (!response.ok) {
+            throw new Error("Error while calling API (button) " + response.status)
+        }
+        console.log("Friend request accepted");
+        return response.text();
+    }).then((data) => {
+        return data;
+    }).catch(error => {
+        console.log(error);
+    });
+}
+
+function addFriend(userIdOfThisPage){
+    return fetch(BASE_URL + API_URL + FRIENDS_URL + "add/" + userIdOfThisPage, {
+        method: 'post', headers: {
+            'Authorization': 'Bearer ' + localStorage.getItem('token'),
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        }
+    }).then((response) => {
+        if (!response.ok) {
+            throw new Error("Error while calling API (button) " + response.status)
+        }
+        console.log("Friend request sent");
+        return response.text();
+    }).then((data) => {
+        return data;
+    }).catch(error => {
+        console.log(error);
+    });
+}
+
+function cancelRequest(userIdOfThisPage){
+    return fetch(BASE_URL + API_URL + FRIENDS_URL + 'removeRequest' + "/" + userIdOfThisPage, {
+        method: 'delete', headers: {'Authorization': 'Bearer ' + localStorage.getItem('token'),
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        }
+    }).then((response) => {
+        if (!response.ok) {
+            throw new Error("Error while calling API (button) " + response.status)
+        }
+        console.log("cancel request");
+        return response.text();
+    }).then((data) => {
+        return data;
+    }).catch(error => {
+        console.log(error);
+    });
+}
+
+function removeFriend(userIdOfThisPage){
+    return fetch(BASE_URL + API_URL + FRIENDS_URL + 'removeFriend' + "/" + userIdOfThisPage, {
+        method: 'delete', headers: {'Authorization': 'Bearer ' + localStorage.getItem('token'),
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        }
+    }).then((response) => {
+        if (!response.ok) {
+            throw new Error("Error while calling API (button) " + response.status)
+        }
+        console.log("friend removed");
+        return response.text();
+    }).then((data) => {
+        return data;
+    }).catch(error => {
+        console.log(error);
+    });
 }
