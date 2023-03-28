a8a7cdea12ea74577923a98960396da2c007821f
Florian Latapie <florian.latapie@etu.unice.fr>
2023-03-28 13:32:51 +0200

    suppression import inutiles back/index.js

--- back/index.js
+++ back/index.js
@@ -2,27 +2,19 @@
 
 // The http module contains methods to handle http queries.
 import * as http from 'http';
-import * as crypto from "crypto";
 
 // Let's import our logic.
 import * as fileQuery from './queryManagers/front.js'
 import * as apiQuery from './queryManagers/api.js'
 import gamedb from "./database/gamedb.js";
 import {Server} from "socket.io";
-import Player from "../front/GameLogic/Player.js";
-import GameEngine from "../front/GameLogic/GameEngine.js";
-import {nextMove, setup} from "./logic/minMaxAi.js";
-import jwt, {verify} from "jsonwebtoken";
-import GameEngineDBUtil from "./object/GameEngineDBUtil.js";
+import {setup} from "./logic/minMaxAi.js";
+import jwt from "jsonwebtoken";
 import {displayACaughtError} from "./util/util.js";
 import {JWTSecretCode} from "./credentials/credentials.js";
 import {AiRoom} from "./play/room/AiRoom.js";
-import {MatchmakingRoom} from "./play/room/MatchmakingRoom.js";
 import {jsonValidator} from "./util/jsonValidator.js";
-import PlayersQueue from "./play/matchmaking/PlayersQueue.js";
 import MatchmakingController from "./play/matchmaking/MatchmakingController.js";
-import userstatsdb from "./database/userstatsdb.js";
-import achievementdb from "./database/achievementdb.js";
 import connectedPlayer from "./socket/PermanentSocketPlayers.js";
 import ChallengeController from "./play/challenge/ChallengeController.js";
 import ConnectedPlayers from "./socket/ConnectedPlayers.js";
@@ -77,15 +69,6 @@ const permanentSocket = io.of("/api/permanent")
 gamedb.removeAllGames().then(() => {
     console.log("Server started, all the games        have been removed from the database, look for /back/index.js to change this behaviour");
 });
-
-/*
-userstatsdb.removeAllStats().then(() => {
-    console.log("Server started, all the user STATS   have been removed from the database, look for /back/index.js to change this behaviour");
-});*/
-
-/*achievementdb.removeAllAchievements().then(() => {
-    console.log("Server started, all the achievements have been removed from the database, look for /back/index.js to change this behaviour");
-});*/
 
 function authenticate(socket, next) {
     let token = socket.handshake.auth.token;
