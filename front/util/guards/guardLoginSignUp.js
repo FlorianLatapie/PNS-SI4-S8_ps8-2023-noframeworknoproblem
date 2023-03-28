1680f9430e248e8d9ef3a2a2ddc719c276c84221
Florian Latapie <florian.latapie@etu.unice.fr>
2023-03-28 20:38:12 +0200

    Correction bug mis sur le dernier commit

--- front/util/guards/guardLoginSignUp.js
+++ front/util/guards/guardLoginSignUp.js
@@ -1,10 +1,13 @@
 import {isTokenValid} from "../jwtParser.js";
-import {LOGIN_URL} from "../path.js";
+import {HOME_URL, LOGIN_URL} from "../path.js";
 import {BASE_URL} from "../frontPath.js";
 
 window.onload = () => {
     let token = localStorage.getItem("token");
-    if (token !== null && isTokenValid(token) || window.location.href.slice(-1) !== "/") {
+    if (window.location.href.slice(-1) !== "/"){
         window.location.href = BASE_URL + LOGIN_URL;
+    }
+    if (token !== null && isTokenValid(token)) {
+        window.location.href = BASE_URL + HOME_URL;
     }
 }
