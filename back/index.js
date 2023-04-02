ead57cc5c7c051ddaca42ec7ac65357316910e98
Florian Latapie <florian.latapie@etu.unice.fr>
2023-04-02 20:08:19 +0200

    Update index.js

--- back/index.js
+++ back/index.js
@@ -64,7 +64,7 @@ const gameSocket = io.of("/api/game");
 const chatSocket = io.of("/api/chat");
 const permanentSocket = io.of("/api/permanent")
 
-let wipeDBOnServerStart = false;
+let wipeDBOnServerStart = true;
 if (wipeDBOnServerStart) {
     gamedb.removeAllGames().then(() => {
         console.log("Server started, all the games                 have been removed from the database, look for /back/index.js to change this behaviour");
