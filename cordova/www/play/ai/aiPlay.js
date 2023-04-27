095a4aae62de05bf62490a8552cd123beecc9cd4
Florian Latapie <florian.latapie@etu.unice.fr>
2023-04-27 17:40:05 +0200

    changement cordana cordova

--- cordova/www/play/ai/aiPlay.js
+++ cordova/www/play/ai/aiPlay.js
@@ -7,7 +7,7 @@ import {drawPopUp} from "../../templates/popUp/play/drawPopUp.js";
 import {parseJwt} from "../../util/jwtParser.js";
 import {winningPopUp} from "../../templates/popUp/play/winningPopUp.js";
 import {losingPopUp} from "../../templates/popUp/play/losingPopUp.js";
-import {drawVibration, losingVibration, winningVibration} from "../../templates/cordana/vibrationsTypes.js";
+import {drawVibration, losingVibration, winningVibration} from "../../templates/cordova/vibrationsTypes.js";
 import {BASE_URL_PAGE} from "../../util/frontPath.js";
 import {HOME_URL} from "../../util/path.js";
 
