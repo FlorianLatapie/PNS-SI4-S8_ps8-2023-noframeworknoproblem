8270217932324ec00cbfa23bc03adfead3977ac3
Quentin <quentin.dubois@etu.univ-cotedazur.fr>
2023-04-27 16:01:34 +0200

    add offline connection into account

--- cordova/www/home/home.js
+++ cordova/www/home/home.js
@@ -19,3 +19,28 @@ document.getElementById("play-local").addEventListener("click", () => {
 document.getElementById("play-matchmaking").addEventListener("click", () => {
     window.location.href = BASE_URL_PAGE + PLAY_MATCHMAKING_URL;
 });
+
+// ----------- Handle offline and online mode -----------
+
+document.addEventListener("offline", onOffline, false);
+document.addEventListener("online", onOnline, false);
+
+function onOffline() {
+    document.getElementById("offline-info").style.display = "block";
+    document.getElementById("play-ai").style.display = "none";
+    document.getElementById("play-matchmaking").style.display = "none";
+    document.getElementsByTagName("burger-menu-open-close")[0].style.display = "none";
+    document.getElementsByTagName("burger-menu")[0].style.display = "none";
+    document.getElementsByTagName("main")[0].style.width = "100vw";
+    document.getElementsByClassName("top-page")[0].style.width = "100%"
+}
+
+function onOnline() {
+    document.getElementById("offline-info").style.display = "none";
+    document.getElementById("play-ai").style.display = "block";
+    document.getElementById("play-matchmaking").style.display = "block";
+    document.getElementsByTagName("burger-menu-open-close")[0].style.display = "flex";
+    document.getElementsByTagName("burger-menu")[0].style.display = "block";
+    document.getElementsByTagName("main")[0].style.width = "auto";
+    document.getElementsByClassName("top-page")[0].style.width = "calc(100% - 79px)";
+}
